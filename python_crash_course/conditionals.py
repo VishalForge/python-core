a8820a87197f27e01ae66fc558db6874a09d1545
VishalForge <satoruvishal@gmail.com>
2026-01-24 10:54:59 +0530

    Create conditionals.py

---
+++ python_crash_course/conditionals.py
@@ -0,0 +1,11 @@
+'''
+ Topic = if statements
+ chapter = 5
+ Source =Python crash course 2nd Edition
+ Practice and understand conditional logic in python
+
+'''
+
+
+# if statements are used to check conditions
+
